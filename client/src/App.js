368a2f82a60a6daf99c85a97658ecf93f67284b5
Karina Williams <coleman_karina@yahoo.com>
2021-12-07 19:15:49 -0800

    made makerequest.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import SaveRequest from "../src/pages/SavedRequest/SaveRequest";
 import RequestBlog from "./components/RequestBlog/RequestBlog";
 import AboutUs from "../src/pages/AboutUs/AboutUs";
 import Contact from "../src/pages/Contact/Contact";
+import makerequest from "../src/pages/makerequest/makerequest";
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
@@ -105,6 +106,7 @@ function App() {
             <Route exact path="/saved" component={SaveRequest} />
             <Route exact path="/about" component={AboutUs} />
             <Route exact path="/contact" component={Contact} />
+            <Route exact path="/makerequest" component={makerequest} />
             <Route render={() => <h1 className="display-2">OOPs! Wrong page!</h1>} />
           </Switch>
           <Footer />
