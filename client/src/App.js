a502075799251d0e9b2e04f40282e67e017ad82a
Karina Williams <coleman_karina@yahoo.com>
2021-12-09 18:47:03 -0800

    added pages and edited files

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import RequestBlog from "./components/RequestBlog/RequestBlog";
 import AboutUs from "../src/pages/AboutUs/AboutUs";
 import Contact from "../src/pages/Contact/Contact";
 import makerequest from "./pages/processrequest";
+import bulletin from "./pages/bulletin/bulletin";
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
@@ -48,103 +49,34 @@ const client = new ApolloClient({
 
 
 
+  function App(){
+    return (
   
-function App() {
-  const [item, setItem] = useState("");
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("items")) || []
-  );
-  const newitem = () => {
-    if (item.trim() !== "") {
-      const newitem = {
-        id: uuidv4(),
-        item: item,
-        color: randomColor({
-          luminosity: "light",
-        }),
-        defaultPos: { x: 100, y: 0 },
-      };
-      setItems((items) => [...items, newitem]);
-      setItem("");
-    } else {
-      alert("Enter a item");
-      setItem("");
-    }
-  };
-
-  const keyPress = (event) => {
-    var code = event.keyCode || event.which;
-    if (code === 13) {
-      newitem();
-    }
-  };
-
-  useEffect(() => {
-    localStorage.setItem("items", JSON.stringify(items));
-  }, [items]);
-
-  const updatePos = (data, index) => {
-    let newArr = [...items];
-    newArr[index].defaultPos = { x: data.x, y: data.y };
-    setItems(newArr);
-  };
-
-  const deleteNote = (id) => {
-    setItems(items.filter((item) => item.id !== id));
-  };
-  return (
-  
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/request" component={RequestBlog} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />            
-            <Route exact path="/saved" component={SaveRequest} />
-            <Route exact path="/about" component={AboutUs} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/makerequest" component={makerequest} />
-            <Route render={() => <h1 className="display-2">OOPs! Wrong page!</h1>} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
-
- return (
-  <div className="App">
-    <div id="new-item">
-      <input
-        value={item}
-        onChange={(e) => setItem(e.target.value)}
-        placeholder="Enter something..."
-        onKeyPress={(e) => keyPress(e)}
-      />
-      <button onClick={newitem}>ENTER</button>
-    </div>
-    {items.map((item, index) => {
-      return (
-        <Draggable
-          key={item.id}
-          defaultPosition={item.defaultPos}
-          onStop={(e, data) => {
-            updatePos(data, index);
-          }}
-        >
-          <div style={{ backgroundColor: item.color }} className="box">
-            {`${item.item}`}
-            <button id="delete" onClick={(e) => deleteNote(item.id)}>
-              X
-            </button>
+      <ApolloProvider client={client}>
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/request" component={RequestBlog} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />            
+              <Route exact path="/saved" component={SaveRequest} />
+              <Route exact path="/about" component={AboutUs} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/makerequest" component={makerequest} />
+              <Route exact path="/bulletin" component={bulletin} />
+              <Route render={() => <h1 className="display-2">OOPs! Wrong page!</h1>} />
+            </Switch>
+            <Footer />
           </div>
-        </Draggable>
-       );
-    })}
-  </div>
-);
-  };
-export default App;
+        </Router>
+      </ApolloProvider>
+    );
+  }
+
+ 
+
+ 
+
+ export default App;
